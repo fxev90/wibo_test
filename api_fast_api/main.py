d55b4127a31231570cc78451385df3f4c3fcd524
Francisco Escalante <fxev90@gmail.com>
2024-03-06 16:26:42 -0400

    Update main.py
    - Updates to handle cors request

--- api_fast_api/main.py
+++ api_fast_api/main.py
@@ -2,6 +2,22 @@ from fastapi import FastAPI
 from routes.UserRoutes import user_router
 from routes.CatRoutes import cat_router
 from routes.AuthRoutes import auth_router
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:4200",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app = FastAPI()
 app.include_router(user_router)
 app.include_router(cat_router)
