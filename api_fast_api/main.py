e95db2c31b7e9ddf27b87d6484617854d414160a
Francisco Escalante <fxev90@gmail.com>
2024-03-06 21:06:48 -0400

    Added metadata to docs

--- api_fast_api/main.py
+++ api_fast_api/main.py
@@ -22,6 +22,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app = FastAPI(
+    title="Wibo test API",
+    description="api for wibo test to connect with mongodb",
+    summary="Aplication for wibo test",
+    version="0.0.1",
+    terms_of_service="Not available",
+    contact={
+        "name": "Francisco Escalante",
+        "url": "https://www.linkedin.com/in/francisco-escalante-5119a3108/",
+        "email": "fxev90@gmail.com",
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+)
+
 app.include_router(user_router)
 app.include_router(cat_router)
 app.include_router(auth_router)
